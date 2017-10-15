83f7b318cd5aea806eb90459a546c5cd25574c49
sbam1 <bamsuseel@gmail.com>
2017-10-14 20:15:30 -0400

    Initial commit

---
+++ src/main/java/com/dileshwory/com/App.java
@@ -0,0 +1,13 @@
+package com.dileshwory.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
